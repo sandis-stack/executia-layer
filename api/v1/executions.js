b99fc348e81734a3a5dceedfb133137523e4fc9c
sandis-stack <sandis@executia.io>
2026-04-25 16:30:17 +0200

    Change Supabase key to SERVICE_ROLE_KEY

--- api/v1/executions.js
+++ api/v1/executions.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
   try {
     const supabaseUrl = process.env.SUPABASE_URL;
-    const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+    const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
     if (!supabaseUrl || !supabaseKey) {
       return res.status(200).json({
